4c3d0591cd5f484b54bd9ecfa131d1df2a80df0e
vildulv <vildulv@gmail.com>
2017-05-13 16:37:56 +0300

    Changes

--- main.rb
+++ main.rb
@@ -1,7 +1,24 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+require './config/environments'
+require './models/model'
+#require 'sinatra/reloader' if development?
 
 get '/' do
   erb :home
+end
+
+post '/submit' do
+  @model = Model.new(params[:model])
+  if @model.save
+    redirect '/models'
+  else
+    "Sorry, there was an error!"
+  end
+end
+
+get '/models' do
+  @models = Model.all
+  erb :models
 end
